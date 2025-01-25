d93a0ffbb70d36749ba1aa4ab64bbeb6310cf934
Santos Alarcón <santosalarcon86@gmail.com>
2025-01-25 11:37:46 +0100

    test: switch to testing-library

--- __tests__/VerticalTimelineElement_test.js
+++ __tests__/VerticalTimelineElement_test.js
@@ -1,65 +1,45 @@
-/**
- * @jest-environment jsdom
- */
 import React from "react";
-import {
-	renderIntoDocument,
-	scryRenderedDOMComponentsWithClass,
-} from "react-dom/test-utils";
-import { mockAllIsIntersecting } from "react-intersection-observer/test-utils";
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
 import VerticalTimelineElement from "../src/VerticalTimelineElement";
 
-beforeEach(() => {
-	mockAllIsIntersecting(true);
-});
-
-describe("VerticalTimeline", () => {
-	it("should have the vertical-timeline-element classname", () => {
-		const component = renderIntoDocument(<VerticalTimelineElement />);
-		scryRenderedDOMComponentsWithClass(component, "vertical-timeline-element");
+describe("VerticalTimelineElement", () => {
+	it("should render with the correct class", () => {
+		render(<VerticalTimelineElement />);
+		expect(screen.getByRole("presentation")).toHaveClass(
+			"vertical-timeline-element",
+		);
 	});
 
-	it("should have the vertical-timeline-element--right classname", () => {
-		const component = renderIntoDocument(
-			<VerticalTimelineElement position="right" />,
-		);
-		scryRenderedDOMComponentsWithClass(
-			component,
+	it("should render with the right position class", () => {
+		render(<VerticalTimelineElement position="right" />);
+		expect(screen.getByRole("presentation")).toHaveClass(
 			"vertical-timeline-element--right",
 		);
 	});
 
-	describe("when children is empty", () => {
-		it("should have the vertical-timeline-element--no-children classname", () => {
-			const component = renderIntoDocument(<VerticalTimelineElement />);
-			scryRenderedDOMComponentsWithClass(
-				component,
-				"vertical-timeline-element--no-children",
-			);
-		});
+	it("should add the no-children class when no children are provided", () => {
+		render(<VerticalTimelineElement />);
+		expect(screen.getByRole("presentation")).toHaveClass(
+			"vertical-timeline-element--no-children",
+		);
+	});
 
-		it("should have the vertical-timeline-element--no-children classname", () => {
-			const componentWithDate = renderIntoDocument(
-				<VerticalTimelineElement date="2018" />,
-			);
-			scryRenderedDOMComponentsWithClass(
-				componentWithDate,
-				"vertical-timeline-element--no-children",
-			);
-		});
+	it("should add the no-children class when only a date is provided", () => {
+		render(<VerticalTimelineElement date="2018" />);
+		expect(screen.getByRole("presentation")).toHaveClass(
+			"vertical-timeline-element--no-children",
+		);
+	});
 
-		it("should not have the vertical-timeline-element--no-children classname", () => {
-			const component = renderIntoDocument(
-				<VerticalTimelineElement>
-					<div>My Child</div>
-				</VerticalTimelineElement>,
-			);
-			expect(
-				scryRenderedDOMComponentsWithClass(
-					component,
-					"vertical-timeline-element--no-children",
-				).length,
-			).toBe(0);
-		});
+	it("should not add the no-children class when children are provided", () => {
+		render(
+			<VerticalTimelineElement>
+				<div>My Child</div>
+			</VerticalTimelineElement>,
+		);
+		expect(screen.getByRole("presentation")).not.toHaveClass(
+			"vertical-timeline-element--no-children",
+		);
 	});
 });
